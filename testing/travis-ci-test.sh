badc5d9316d07340c33399db7d27346d8827514a
Eric Garrido <ekg@google.com>
2017-01-04 16:28:09 +0000

    Add support for testing on OS X via Travis

--- testing/travis-ci-test.sh
+++ testing/travis-ci-test.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 # Run tests under Travis for continuous integration.
 
+if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then 
+  export DISPLAY=:99.0
+  sh -e /etc/init.d/xvfb start 
+  sleep 3  # give xvfb some time to start
+fi
+
+# Download the Selenium and ChromeDriver binaries.
 go get -d -v
 pushd vendor
 go get -d -v
 go run init.go
 popd
-# Travis has firefox already installed.
+
+# Run the tests. Travis has firefox already installed.
 go test -test.v -test.run=TestFirefox --start_frame_buffer=false
